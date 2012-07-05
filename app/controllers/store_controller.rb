414d773ca90c74f042f612bcddc68b2113ea44ea
William Broughton <william.broughton@ymail.com>
2012-07-05 15:07:41 -0400

    Iteration E1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,10 @@ class StoreController < ApplicationController
   def index
     @time = Time.now
     @products = Product.order(:title)
+    
+    @counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter+=1
+    session[:counter] = @counter
+    @shown_message = "You've been here #{@counter} times" if session[:counter] >5
   end
 end
